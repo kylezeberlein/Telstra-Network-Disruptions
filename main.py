48ef85ff0df62027ac0abc9f50269f81727befb6
Kyle Zeberlein <kylezeberlein@gmail.com>
2018-02-13 19:32:49 -0500

    First commit. Set up directory

---
+++ main.py
@@ -0,0 +1,26 @@
+'''
+	Kyle Zeberlein
+	Telstra Network Disruptions
+	https://www.kaggle.com/c/telstra-recruiting-network
+'''
+
+# imports
+import pandas as pd
+
+
+# read data
+train = pd.read_csv('data//train.csv')
+
+# explore data
+print(train.columns)
+
+
+
+
+
+
+
+
+
+
+
